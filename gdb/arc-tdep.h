e87754e2905d81cffa1d3e611ff64d268cb7ffd6
Jeremy Bennett <jeremy.bennett@embecosm.com>
2012-12-12 18:04:35 +0000

    Merge from 6.8 branch.
    At top level:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual merge from GDB 6.8 branch.
    
    	* README.arcgdb: Merged as new file.
    	* config.sub: Merged.
    
    In config:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual Merge from GDB 6.8 branch.
    
    In gdb:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual merge from GDB 6.8 branch.
    
    	* ChangeLog.ARC, arc-architecture.c, arc-architecture.h,
    	* arc-aux-registers.c, arc-aux-registers.h, arc-board.c,
    	* arc-board.h, arc-embed.c, arc-gpio.c, arc-gpio.h,
    	* arc-jtag-actionpoints.c, arc-jtag-actionpoints.h,
    	* arc-jtag-fileio.c, arc-jtag-fileio.h, arc-jtag-ops.c,
    	* arc-jtag-ops.h, arc-jtag-tdep.c, arc-jtag-tdep.h, arc-jtag.c,
    	* arc-jtag.h, arc-linux-tdep.c, arc-linux-tdep.h,
    	* arc-regnums-defs.h, arc-support.h, arc-tdep.c, arc-tdep.h,
    	* features/arc-a5-cpu.xml, features/arc-aux-registers.dtd,
    	* features/arc600-cpu.xml, features/arc700-cpu.xml,
    	* regformats/reg-arc.dat: Merged as new files.
    	* configure.tgt: Merged.
    
    In gdb/gdbserver:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual merge from GDB 6.8 branch.
    
    	* linux-arc-low.c: Merged as new file.
    	* Makefile.in, configure.src: Merged.
    
    In sim:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual merge from GDB 6.8 branch.
    
    	* arc: Merged as new directory.
    
    In sim/arc:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual merge from GDB 6.8 branch.
    
    	* ChangeLog.ARC, Makefile.in, arc-sim-registers.c,
    	* arc-sim-registers.h, arc-sim.h, arc5.c, arc6.c, arc7.c, arch.c,
    	* arch.h, config.in, configure, configure.ac, cpu5.c, cpu5.h,
    	* cpu6.c, cpu6.h, cpu7.c, cpu7.h, cpuall.h, decode5.c, decode5.h,
    	* decode6.c, decode6.h, decode7.c, decode7.h, devices.c, mloop5.in,
    	* mloop6.in, mloop7.in, model5.c, model6.c, model7.c, sem5-switch.c,
    	* sem5.c, sem6-switch.c, sem6.c, sem7-switch.c, sem7.c, sim-if.c,
    	* sim-main.h, syscall.h, tconfig.in, traps-linux.c, traps.c: Merge
    	as new files.

---
+++ gdb/arc-tdep.h
@@ -0,0 +1,150 @@
+/* Target dependent code for ARC processor family, for GDB, the GNU debugger.
+
+   Copyright 2005 Free Software Foundation, Inc.
+   Copyright 2009-2012 Synopsys Inc.
+
+   Contributed by Codito Technologies Pvt. Ltd. (www.codito.com) on behalf of
+   Synopsys Inc.
+
+   Authors: 
+      Soam Vasani          <soam.vasani@codito.com>
+      Ramana Radhakrishnan <ramana.radhakrishnan@codito.com> 
+      Richard Stuckey      <richard.stuckey@arc.com>
+
+   This file is part of GDB.
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+*/
+
+/******************************************************************************/
+/*                                                                            */
+/* Outline:                                                                   */
+/*     This header file defines some target-dependent information which is    */
+/*     specific to the ARC gdb port.                                          */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef ARC_TDEP_H
+#define ARC_TDEP_H
+
+/* ARC header files */
+#include "arc-support.h"
+
+
+#define ARC_PC_REGNUM       (gdbarch_pc_regnum       (current_gdbarch))
+#define ARC_NUM_REGS        (gdbarch_num_regs        (current_gdbarch))
+#define ARC_NUM_PSEUDO_REGS (gdbarch_num_pseudo_regs (current_gdbarch))
+#define ARC_TOTAL_REGS      (ARC_NUM_REGS + ARC_NUM_PSEUDO_REGS)
+
+
+#define ARC_MAX_CORE_REGS                      64
+#define ARC_FIRST_EXTENSION_CORE_REGISTER      32
+#define ARC_LAST_EXTENSION_CORE_REGISTER       59
+#define ARC_NUM_EXTENSION_CORE_REGS            (ARC_LAST_EXTENSION_CORE_REGISTER - ARC_FIRST_EXTENSION_CORE_REGISTER + 1)
+#define ARC_NUM_STANDARD_CORE_REGS             (ARC_MAX_CORE_REGS - ARC_NUM_EXTENSION_CORE_REGS)
+
+
+#define IS_EXTENSION_CORE_REGISTER(hw_regnum)  (ARC_FIRST_EXTENSION_CORE_REGISTER <= (hw_regnum) && (hw_regnum) <= ARC_LAST_EXTENSION_CORE_REGISTER)
+
+
+/* ARC processor ABI-related registers:
+ *
+ *    R0  .. R7 are the registers used to pass arguments in function calls
+ *    R13 .. R26 are the callee-saved registers
+ *    when a return value is stored in registers it is in either R0 or in the pair (R0,R1).
+ */
+
+#define ARC_ABI_GLOBAL_POINTER                 26
+#define ARC_ABI_FRAME_POINTER                  27
+#define ARC_ABI_STACK_POINTER                  28
+
+#define ARC_ABI_FIRST_CALLEE_SAVED_REGISTER    13
+#define ARC_ABI_LAST_CALLEE_SAVED_REGISTER     26
+
+#define ARC_ABI_FIRST_ARGUMENT_REGISTER         0
+#define ARC_ABI_LAST_ARGUMENT_REGISTER          7
+
+#define ARC_ABI_RETURN_REGNUM                   0
+#define ARC_ABI_RETURN_LOW_REGNUM               0
+#define ARC_ABI_RETURN_HIGH_REGNUM              1
+
+#define IS_ARGUMENT_REGISTER(hw_regnum)         (ARC_ABI_FIRST_ARGUMENT_REGISTER <= (hw_regnum) && (hw_regnum) <= ARC_ABI_LAST_ARGUMENT_REGISTER)
+
+
+/* this type is completed in the files arc-jtag-tdep.h and arc-linux-tdep.h,
+ * as apppropriate for the arc-elf2 and arc-uclinux builds of gdb
+ */
+typedef struct arc_variant_info ARC_VariantsInfo;
+
+
+#define REGISTER_NOT_PRESENT   (-1)   // special value for sc_reg_offset[reg]
+
+
+/* this structure holds target-dependent information
+ *
+ * N.B. this type is used in the target-independent gdb code, but it is treated
+ *      as an opaque (or private) type: the only use of it is by pointers to
+ *      objects of this type (passed as parameters or returned as results, or
+ *      held in other structures); it is only the ARC-specific modules that
+ *      have knowledge of the structure of this type and access its fields.
+ */
+struct gdbarch_tdep
+{
+    /* Detect sigtramp.  */
+    Boolean (*is_sigtramp) (struct frame_info*);
+  
+    /* Get address of sigcontext for sigtramp.  */
+    CORE_ADDR (*sigcontext_addr) (struct frame_info*);
+
+    /* Offset of registers in `struct sigcontext'. */
+    const int*   sc_reg_offset;
+    unsigned int sc_num_regs;
+
+    /* In our linux target, gdbarch_pc_regnum points to stop_pc, which is a
+       register that is made up by the kernel and does not actually exist.
+       stop_pc is NOT saved in the sigcontext; what is saved is the ret
+       "register".  Since ret is a linux-only register, its regnum is visible
+       only in arc-linux-tdep.c; hence initialize pc_regnum_in_sigcontext in
+       arc-linux-tdep.c.  */
+    int pc_regnum_in_sigcontext;
+
+    /* Returns 0, 1, or -1:
+     *    0 means the register is not in the group.
+     *    1 means the register is in the group.
+     *   -1 means the tdep has nothing to say about this register and group.
+     */
+    int (*register_reggroup_p) (int regnum, struct reggroup* group);
+  
+    /* Breakpoint instruction to be used */
+    const unsigned char* breakpoint_instruction;
+    unsigned int         breakpoint_size;
+
+    /* For stopping backtraces.  */
+    CORE_ADDR lowest_pc;
+  
+    /* ARC processor variant information (may be NULL). */
+    ARC_VariantsInfo* processor_variant_info;
+};
+
+
+void _initialize_arc_tdep (void);
+
+
+/* utility functions used by other ARC-specific modules */
+
+void arc_initialize_disassembler(struct disassemble_info* info);
+
+#endif /* ARC_TDEP_H */
+/******************************************************************************/
