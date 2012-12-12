e32ce97d486bbbba968ed01eeb82b9f94b4696f9
Jeremy Bennett <jeremy.bennett@embecosm.com>
2012-12-12 18:04:35 +0000

    Merge from 6.8 branch.
    At top level:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual merge from GDB 6.8 branch.
    
    	* README.arcgdb: Merged as new file.
    	* config.sub: Merged.
    
    In config:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual Merge from GDB 6.8 branch.
    
    In gdb:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual merge from GDB 6.8 branch.
    
    	* ChangeLog.ARC, arc-architecture.c, arc-architecture.h,
    	* arc-aux-registers.c, arc-aux-registers.h, arc-board.c,
    	* arc-board.h, arc-embed.c, arc-gpio.c, arc-gpio.h,
    	* arc-jtag-actionpoints.c, arc-jtag-actionpoints.h,
    	* arc-jtag-fileio.c, arc-jtag-fileio.h, arc-jtag-ops.c,
    	* arc-jtag-ops.h, arc-jtag-tdep.c, arc-jtag-tdep.h, arc-jtag.c,
    	* arc-jtag.h, arc-linux-tdep.c, arc-linux-tdep.h,
    	* arc-regnums-defs.h, arc-support.h, arc-tdep.c, arc-tdep.h,
    	* features/arc-a5-cpu.xml, features/arc-aux-registers.dtd,
    	* features/arc600-cpu.xml, features/arc700-cpu.xml,
    	* regformats/reg-arc.dat: Merged as new files.
    	* configure.tgt: Merged.
    
    In gdb/gdbserver:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual merge from GDB 6.8 branch.
    
    	* linux-arc-low.c: Merged as new file.
    	* Makefile.in, configure.src: Merged.
    
    In sim:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual merge from GDB 6.8 branch.
    
    	* arc: Merged as new directory.
    
    In sim/arc:
    
    2012-12-12  Jeremy Bennett <jeremy.bennett@embecosm.com>
    
    	Manual merge from GDB 6.8 branch.
    
    	* ChangeLog.ARC, Makefile.in, arc-sim-registers.c,
    	* arc-sim-registers.h, arc-sim.h, arc5.c, arc6.c, arc7.c, arch.c,
    	* arch.h, config.in, configure, configure.ac, cpu5.c, cpu5.h,
    	* cpu6.c, cpu6.h, cpu7.c, cpu7.h, cpuall.h, decode5.c, decode5.h,
    	* decode6.c, decode6.h, decode7.c, decode7.h, devices.c, mloop5.in,
    	* mloop6.in, mloop7.in, model5.c, model6.c, model7.c, sem5-switch.c,
    	* sem5.c, sem6-switch.c, sem6.c, sem7-switch.c, sem7.c, sim-if.c,
    	* sim-main.h, syscall.h, tconfig.in, traps-linux.c, traps.c: Merge
    	as new files.

---
+++ gdb/arc-board.h
@@ -0,0 +1,71 @@
+/* Target dependent code for ARC processor family, for GDB, the GNU debugger.
+
+   Copyright 2009 Free Software Foundation, Inc.
+   Copyright 2009-2012 Synopsys Inc.
+
+   Contributed by  ARC International (www.arc.com), now part of Synopsys Inc.
+
+   Authors:
+      Phil Barnard    <phil.barnard@arc.com>
+      Richard Stuckey <richard.stuckey@arc.com>
+
+   This file is part of GDB.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+/******************************************************************************/
+/*                                                                            */
+/* Outline:                                                                   */
+/*     This header file defines operations for controlling an ARC target      */
+/*     board.                                                                 */
+/*                                                                            */
+/*     These operations are:                                                  */
+/*        1) configuring ("blasting") an FPGA target with the contents of an  */
+/*           XBF file;                                                        */
+/*        2) checking whether a target has been so configured; and            */
+/*        3) setting the clock frequency of the target.                       */
+/*                                                                            */
+/* Notes:                                                                     */
+/*     The arc_blast_board function implements an ARC-specific command; hence */
+/*     its 'args' parameter contains data entered by the debugger user, which */
+/*     must be checked for validity.                                          */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef ARC_BOARD_H
+#define ARC_BOARD_H
+
+/* ARC header files */
+#include "arc-support.h"
+
+
+typedef enum
+{
+    INACCESSIBLE,
+    CONFIGURED,
+    UNCONFIGURED
+} FPGA_Status;
+
+
+void arc_blast_board (char *args, int from_tty);
+
+void arc_reset_board(void);
+
+FPGA_Status arc_is_FPGA_configured (void);
+
+
+#endif /* ARC_BOARD_H */
+/******************************************************************************/
