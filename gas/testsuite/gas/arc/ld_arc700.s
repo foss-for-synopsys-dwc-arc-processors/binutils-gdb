855fb261e6e4b59d08ef3e22ec789f2301e9fddf
Simon Cook <simon.cook@embecosm.com>
2013-03-04 10:13:57 +0000

    Changes to support big endian ARC and consistent testing with both toolchains.
    gas/
            * testsuite/gas/all/redef3.d: Replace arc-*-* with arc*-*-*.
            * testsuite/gas/arc/ex_arc700.s: Likewise.
            * testsuite/gas/arc/ld_arc700.s: Likewise.
            * testsuite/gas/arc/warn.s: Likewise.
            * testsuite/gas/lns/lns.exp: Likewise.
    
    ld/
            * Makefile.am (ALL_EMULATION_SOURCES): Added big endian sources.
            * Makefile.in: Regenerated.
            * configure.tgt: Added big endian ARC configurations.
            * emulparams/arcebelf.sh, emulparams/arcebelf_prof.sh,
            emulparams/arceblinux.sh, emulparams/arceblinux_prof.sh: New files.
            * testsuite/ld-discard/extern.d: Replace arc-*-* with arc*-*-*.
            * testsuite/ld-discard/start.d: Likewise.
            * testsuite/ld-discard/static.d: Likewise.
            * testsuite/ld-elf/group1.d: Likewise.
            * testsuite/ld-elf/group8a.d: Likewise.
            * testsuite/ld-elf/group9a.d: Likewise.
            * testsuite/ld-elf/group9b.d: Likewise.
            * testsuite/ld-elf/linkonce2.d: Likewise.
            * testsuite/ld-elf/merge.d: Likewise.
            * testsuite/ld-elf/merge2.d: Likewise.
            * testsuite/ld-elf/orphan-region.d: Likewise.
            * testsuite/ld-elf/orphan.d: Likewise.
            * testsuite/ld-elf/orphan3.d: Likewise.
            * testsuite/ld-elf/pr12851.d: Likewise.
            * testsuite/ld-elf/pr12975.d: Likewise.
            * testsuite/ld-elf/pr13195.d: Likewise.
            * testsuite/ld-elf/pr349.d: Likewise.
            * testsuite/ld-elf/sec64k.exp: Likewise.
            * testsuite/ld-elf/warn1.d: Likewise.
            * testsuite/ld-elf/warn2.d: Likewise.
            * testsuite/ld-elf/warn3.d: Likewise.
            * testsuite/lib/ld-lib.exp: Likewise.

--- gas/testsuite/gas/arc/ld_arc700.s
+++ gas/testsuite/gas/arc/ld_arc700.s
@@ -16,7 +16,7 @@
 ; along with this program; see the file COPYING3.  If not see
 ; <http://www.gnu.org/licenses/>.  */
 ;
-; { dg-do assemble { target arc-*-* } }
+; { dg-do assemble { target arc*-*-* } }
 
 	ld	lp_count,[sp,4]	; { dg-error "ld operand error" }
 	
