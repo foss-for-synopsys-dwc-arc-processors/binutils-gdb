9230487a83ab310b4817154252dbd9fb3cea7eef
Simon Cook <simon.cook@embecosm.com>
2013-03-12 16:15:16 +0000

    2013-03-12  Simon Cook  <simon.cook@embecosm.com>
            * emulparams/arcelf.sh, emulparams/arcebelf.sh: Change TEMPLATE_NAME
            to elf32.
    
    Conflicts:
    	ld/emulparams/arcelf.sh

--- ld/emulparams/arcelf.sh
+++ ld/emulparams/arcelf.sh
@@ -1,10 +1,6 @@
 SCRIPT_NAME=elfarc
 TEMPLATE_NAME=elf32
-if [ "x${ARC_ENDIAN}" = "xbig" ]; then
-  OUTPUT_FORMAT="elf32-bigarc"
-else
-  OUTPUT_FORMAT="elf32-littlearc"
-fi
+OUTPUT_FORMAT="elf32-littlearc"
 LITTLE_OUTPUT_FORMAT="elf32-littlearc"
 BIG_OUTPUT_FORMAT="elf32-bigarc"
 # leave room for vector table, 32 vectors * 8 bytes
