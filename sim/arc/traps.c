367154077146c63a8ca5f21a2f4d6db4a9be1ebe
Joern Rennecke <joern.rennecke@embecosm.com>
2013-03-17 07:36:50 +0000

    traps.c: Amend Copyright years.

--- sim/arc/traps.c
+++ sim/arc/traps.c
@@ -1,5 +1,6 @@
 /* arc exception, interrupt, and trap (EIT) support
-   Copyright (C) 1998, 2003, 2004, 2005 Free Software Foundation, Inc.
+   Copyright (C) 1998, 2003, 2004, 2005, 2012, 2013
+   Free Software Foundation, Inc.
 
    This file is part of GDB, the GNU debugger.
 
