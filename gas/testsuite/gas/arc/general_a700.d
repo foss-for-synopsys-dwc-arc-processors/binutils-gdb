c80e8fea8e531164249d94c0c57615bd94151f52
Simon Cook <simon.cook@embecosm.com>
2013-03-28 09:46:46 +0000

    * gas/arc/dsp1.d, gas/arc/dsp2.d, gas/arc/general_a700.d,         mpy_a700.d, prefetch.d, sub_s.d: Fix patterns to check for tests.

--- gas/testsuite/gas/arc/general_a700.d
+++ gas/testsuite/gas/arc/general_a700.d
@@ -1,5 +1,5 @@
 #as: -mARC700
-#objdump: -dr  --prefix-addresses
+#objdump: -dr
 #name: general_a700
 
 # Test the @OC@ insn.
@@ -7,23 +7,25 @@
 .*: +file format elf32-.*arc
 
 Disassembly of section .text:
-0x00000000 236f003f     sync       
-0x00000004 242f003f     rtie       
-0x00000008 256f003f     brk        
-0x0000000c 216f003f     sleep      
-0x00000010         0000783e     trap_s     1
-0x00000012 226f003f     swi        
-0x00000016 216f003f     sleep      
-0x0000001a 216f003f     sleep      
-0x0000001e 216f007f     sleep      1
-0x00000022 216f00bf     sleep      2
-0x00000026 216f00ff     sleep      3
-0x0000002a 216f013f     sleep      4
-0x0000002e         000079e0     unimp_s    
-0x00000030 202f004c     ex         r0,\[r1\]
-0x00000034 202f804c     ex.di      r0,\[r1\]
-0x00000038 202f0f8c     ex         r0,\[0xdeadbeef\]
-0x00000040 202f8f8c     ex.di      r0,\[0xdeadbeef\]
+
+00000000 <.text>:
+   0:	6f 23 3f 00             	sync       
+   4:	2f 24 3f 00             	rtie       
+   8:	6f 25 3f 00             	brk        
+   c:	6f 21 3f 00             	sleep      
+  10:	3e 78                   	trap_s     1
+  12:	6f 22 3f 00             	swi        
+  16:	6f 21 3f 00             	sleep      
+  1a:	6f 21 3f 00             	sleep      
+  1e:	6f 21 7f 00             	sleep      1
+  22:	6f 21 bf 00             	sleep      2
+  26:	6f 21 ff 00             	sleep      3
+  2a:	6f 21 3f 01             	sleep      4
+  2e:	e0 79                   	unimp_s    
+  30:	2f 20 4c 00             	ex         r0,\[r1\]
+  34:	2f 20 4c 80             	ex.di      r0,\[r1\]
+  38:	2f 20 8c 0f ad de ef be 	ex         r0,\[0xdeadbeef\]
+  40:	2f 20 8c 8f ad de ef be 	ex.di      r0,\[0xdeadbeef\]
 
 
 
--- gas/testsuite/gas/arc/mpy_a700.d
+++ gas/testsuite/gas/arc/mpy_a700.d
@@ -1,5 +1,5 @@
-#as: -mARC700 -mmpy
-#objdump: -dr  --prefix-addresses
+#as: -mARC700
+#objdump: -dr
 #name: @OC@
 
 # Test the @OC@ insn.
@@ -7,43 +7,45 @@
 .*: +file format elf32-.*arc
 
 Disassembly of section .text:
-0x00000000 21@MUL_IC+0@0080     @OC@     @sp+6@r0,r1,r2
-0x00000004 26@MUL_IC+0@7040     @OC@     @sp+6@r0,0xdeadbeef,r1
-0x0000000c 21@MUL_IC+0@0f80     @OC@     @sp+6@r0,r1,0xdeadbeef
-0x00000014 21@MUL_IC+0@00be     @OC@     @sp+6@0,r1,r2
-0x00000018 26@MUL_IC+0@707e     @OC@     @sp+6@0,0xdeadbeef,r1
-0x00000020 21@MUL_IC+0@0fbe     @OC@     @sp+6@0,r1,0xdeadbeef
-0x00000028 26@MUL_IC+0@7f80     @OC@     @sp+6@r0,0xdeadbeef,0xdeadbeef
-0x00000030 26@MUL_IC+0@7fbe     @OC@     @sp+6@0,0xdeadbeef,0xdeadbeef
-0x00000038 21@MUL_IC+0@8080     @OC@.f   @sp+6@r0,r1,r2
-0x0000003c 26@MUL_IC+0@f040     @OC@.f   @sp+6@r0,0xdeadbeef,r1
-0x00000044 21@MUL_IC+0@8f80     @OC@.f   @sp+6@r0,r1,0xdeadbeef
-0x0000004c 21@MUL_IC+0@80be     @OC@.f   @sp+6@0,r1,r2
-0x00000050 26@MUL_IC+0@f07e     @OC@.f   @sp+6@0,0xdeadbeef,r1
-0x00000058 21@MUL_IC+0@8fbe     @OC@.f   @sp+6@0,r1,0xdeadbeef
-0x00000060 26@MUL_IC+0@ff80     @OC@.f   @sp+6@r0,0xdeadbeef,0xdeadbeef
-0x00000068 26@MUL_IC+0@ffbe     @OC@.f   @sp+6@0,0xdeadbeef,0xdeadbeef
-0x00000070 21@MUL_IC+1@0040     @OC@     @sp+6@r0,r1,1
-0x00000074 21@MUL_IC+1@007e     @OC@     @sp+6@0,r1,1
-0x00000078 26@MUL_IC+1@7040     @OC@     @sp+6@r0,0xdeadbeef,1
-0x00000080 26@MUL_IC+1@707e     @OC@     @sp+6@0,0xdeadbeef,1
-0x00000088 21@MUL_IC+1@8040     @OC@.f   @sp+6@r0,r1,1
-0x0000008c 21@MUL_IC+1@807e     @OC@.f   @sp+6@0,r1,1
-0x00000090 26@MUL_IC+1@f040     @OC@.f   @sp+6@r0,0xdeadbeef,1
-0x00000098 26@MUL_IC+1@f07e     @OC@.f   @sp+6@0,0xdeadbeef,1
-0x000000a0 20@MUL_IC+2@0fff     @OC@     @sp+6@r0,r0,-1
-0x000000a4 26@MUL_IC+2@7fff     @OC@     @sp+6@0,0xdeadbeef,-1
-0x000000ac 20@MUL_IC+2@8fff     @OC@.f   @sp+6@r0,r0,-1
-0x000000b0 26@MUL_IC+2@ffff     @OC@.f   @sp+6@0,0xdeadbeef,-1
-0x000000b8 21@MUL_IC+3@0081     @OC@.z   @sp+6@r1,r1,r2
-0x000000bc 26@MUL_IC+3@7041     @OC@.z   @sp+6@0,0xdeadbeef,r1
-0x000000c4 21@MUL_IC+3@0f81     @OC@.z   @sp+6@r1,r1,0xdeadbeef
-0x000000cc 26@MUL_IC+3@7f81     @OC@.z   @sp+6@0,0xdeadbeef,0xdeadbeef
-0x000000d4 21@MUL_IC+3@8081     @OC@.z.f @sp+6@r1,r1,r2
-0x000000d8 26@MUL_IC+3@f041     @OC@.z.f @sp+6@0,0xdeadbeef,r1
-0x000000e0 21@MUL_IC+3@8f81     @OC@.z.f @sp+6@r1,r1,0xdeadbeef
-0x000000e8 26@MUL_IC+3@ff81     @OC@.z.f @sp+6@0,0xdeadbeef,0xdeadbeef
-0x000000f0 21@MUL_IC+3@00a1     @OC@.z   @sp+6@r1,r1,2
-0x000000f4 26@MUL_IC+3@70a1     @OC@.z   @sp+6@0,0xdeadbeef,2
-0x000000fc 21@MUL_IC+3@80a1     @OC@.z.f @sp+6@r1,r1,2
-0x00000100 26@MUL_IC+3@f0a1     @OC@.z.f @sp+6@0,0xdeadbeef,2
+
+00000000 <.text>:
+   0:	@MUL_IC+0@ 21 80 00             	@OC@     @sp+6@r0,r1,r2
+   4:	@MUL_IC+0@ 26 40 70 ad de ef be 	@OC@     @sp+6@r0,0xdeadbeef,r1
+   c:	@MUL_IC+0@ 21 80 0f ad de ef be 	@OC@     @sp+6@r0,r1,0xdeadbeef
+  14:	@MUL_IC+0@ 21 be 00             	@OC@     @sp+6@0,r1,r2
+  18:	@MUL_IC+0@ 26 7e 70 ad de ef be 	@OC@     @sp+6@0,0xdeadbeef,r1
+  20:	@MUL_IC+0@ 21 be 0f ad de ef be 	@OC@     @sp+6@0,r1,0xdeadbeef
+  28:	@MUL_IC+0@ 26 80 7f ad de ef be 	@OC@     @sp+6@r0,0xdeadbeef,0xdeadbeef
+  30:	@MUL_IC+0@ 26 be 7f ad de ef be 	@OC@     @sp+6@0,0xdeadbeef,0xdeadbeef
+  38:	@MUL_IC+0@ 21 80 80             	@OC@.f   @sp+6@r0,r1,r2
+  3c:	@MUL_IC+0@ 26 40 f0 ad de ef be 	@OC@.f   @sp+6@r0,0xdeadbeef,r1
+  44:	@MUL_IC+0@ 21 80 8f ad de ef be 	@OC@.f   @sp+6@r0,r1,0xdeadbeef
+  4c:	@MUL_IC+0@ 21 be 80             	@OC@.f   @sp+6@0,r1,r2
+  50:	@MUL_IC+0@ 26 7e f0 ad de ef be 	@OC@.f   @sp+6@0,0xdeadbeef,r1
+  58:	@MUL_IC+0@ 21 be 8f ad de ef be 	@OC@.f   @sp+6@0,r1,0xdeadbeef
+  60:	@MUL_IC+0@ 26 80 ff ad de ef be 	@OC@.f   @sp+6@r0,0xdeadbeef,0xdeadbeef
+  68:	@MUL_IC+0@ 26 be ff ad de ef be 	@OC@.f   @sp+6@0,0xdeadbeef,0xdeadbeef
+  70:	@MUL_IC+1@ 21 40 00             	@OC@     @sp+6@r0,r1,1
+  74:	@MUL_IC+1@ 21 7e 00             	@OC@     @sp+6@0,r1,1
+  78:	@MUL_IC+1@ 26 40 70 ad de ef be 	@OC@     @sp+6@r0,0xdeadbeef,1
+  80:	@MUL_IC+1@ 26 7e 70 ad de ef be 	@OC@     @sp+6@0,0xdeadbeef,1
+  88:	@MUL_IC+1@ 21 40 80             	@OC@.f   @sp+6@r0,r1,1
+  8c:	@MUL_IC+1@ 21 7e 80             	@OC@.f   @sp+6@0,r1,1
+  90:	@MUL_IC+1@ 26 40 f0 ad de ef be 	@OC@.f   @sp+6@r0,0xdeadbeef,1
+  98:	@MUL_IC+1@ 26 7e f0 ad de ef be 	@OC@.f   @sp+6@0,0xdeadbeef,1
+  a0:	@MUL_IC+2@ 20 ff 0f             	@OC@     @sp+6@r0,r0,-1
+  a4:	@MUL_IC+2@ 26 ff 7f ad de ef be 	@OC@     @sp+6@0,0xdeadbeef,-1
+  ac:	@MUL_IC+2@ 20 ff 8f             	@OC@.f   @sp+6@r0,r0,-1
+  b0:	@MUL_IC+2@ 26 ff ff ad de ef be 	@OC@.f   @sp+6@0,0xdeadbeef,-1
+  b8:	@MUL_IC+3@ 21 81 00             	@OC@.z   @sp+6@r1,r1,r2
+  bc:	@MUL_IC+3@ 26 41 70 ad de ef be 	@OC@.z   @sp+6@0,0xdeadbeef,r1
+  c4:	@MUL_IC+3@ 21 81 0f ad de ef be 	@OC@.z   @sp+6@r1,r1,0xdeadbeef
+  cc:	@MUL_IC+3@ 26 81 7f ad de ef be 	@OC@.z   @sp+6@0,0xdeadbeef,0xdeadbeef
+  d4:	@MUL_IC+3@ 21 81 80             	@OC@.z.f @sp+6@r1,r1,r2
+  d8:	@MUL_IC+3@ 26 41 f0 ad de ef be 	@OC@.z.f @sp+6@0,0xdeadbeef,r1
+  e0:	@MUL_IC+3@ 21 81 8f ad de ef be 	@OC@.z.f @sp+6@r1,r1,0xdeadbeef
+  e8:	@MUL_IC+3@ 26 81 ff ad de ef be 	@OC@.z.f @sp+6@0,0xdeadbeef,0xdeadbeef
+  f0:	@MUL_IC+3@ 21 a1 00             	@OC@.z   @sp+6@r1,r1,2
+  f4:	@MUL_IC+3@ 26 a1 70 ad de ef be 	@OC@.z   @sp+6@0,0xdeadbeef,2
+  fc:	@MUL_IC+3@ 21 a1 80             	@OC@.z.f @sp+6@r1,r1,2
+ 100:	@MUL_IC+3@ 26 a1 f0 ad de ef be 	@OC@.z.f @sp+6@0,0xdeadbeef,2
