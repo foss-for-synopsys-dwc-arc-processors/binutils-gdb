0e2bba6e782edb598239a646b4fb8da857c2e4d8
Simon Cook <simon.cook@embecosm.com>
2013-04-11 14:03:57 +0100

    Merge in EM working branch
    Conflicts:
    	ChangeLog.ARC
    	bfd/ChangeLog.ARC
    	bfd/cpu-arc.c
    	bfd/elf32-arc.c
    	gas/ChangeLog.ARC
    	gas/config/tc-arc.c
    	opcodes/ChangeLog.ARC
    	opcodes/arc-opc-old.c

--- bfd/bfd-in2.h
+++ bfd/bfd-in2.h
@@ -2115,6 +2115,7 @@ enum bfd_architecture
 #define bfd_mach_arc_arc600    2
 #define bfd_mach_arc_arc601    4
 #define bfd_mach_arc_arc700    3
+#define bfd_mach_arc_arcv2     5
  bfd_arch_m32c,     /* Renesas M16C/M32C.  */
 #define bfd_mach_m16c        0x75
 #define bfd_mach_m32c        0x78
