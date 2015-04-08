cab3a2aff1aa82f93dcae2be7a1e658d22f77e40
Anton Kolesov <Anton.Kolesov@synopsys.com>
2015-04-08 15:40:07 +0300

    Do not return inline frame that doesn't have previous frame
    GDB experiences an internal error (failed assert) when an outermost frame is
    also an inline frame, because it's not possible to compute frame_id for inline frame
    without having a previous frame it and GDB assumes that inline frame always
    has a previous non-inline frame. I'm not aware of situation where this
    can happen naturally, however it is possible to create such situation
    artifically via setting ProgramCounter and Return-ProgramCounter registers. For example
    assume there are functions a() and b(), where a() is inlined into b(). To
    trigger an error one has to set ProgramCounter to address inside b(), but
    outside of a(), while Return-ProgramCounter (for example LR/R14 on ARM) should be set to
    address inside the inlined a(). As a result when unwinding the trace:
    
    1. GDB will successfully build frame 0 for the program counter, that
    frame will be for function b.
    2. GDB will build a raw frame 1 for the value stored in
    Return-ProgramCounter. Because that is inside inlined function (that is
    coming from debug information), GDB cannot compute frame_id for the inline
    frame 1 directly - it should create a first previous non-inline frame for
    that, frame 2 if there are no nested inline functions.
    In our case that would be a frame 2 for function b(). So this frame 2 is created,
    however its frame_id is equal to frame_id of the first frame, so
    get_prev_frame_if_no_cycle() will discard it and will return NULL, with stop_reason =
    UNWIND_SAME_ID. Consequently it will not be possible to calculate frame_id
    of inline frame, so safest way is to discard that frame as invalid.
    
    Example of GDB stack trace when error happens:
    
    0  internal_error () at gdb/common/errors.c:51
    1  inline_frame_this_id () at gdb/inline-frame.c:167
    2  compute_frame_id () at gdb/frame.c:458
    3  get_prev_frame_if_no_cycle () at gdb/frame.c:1780
    4  get_prev_frame_always_1 () at gdb/frame.c:1954
    5  get_prev_frame_always () at gdb/frame.c:1971
    6  get_prev_frame () at gdb/frame.c:2212
    7  frame_info () at gdb/stack.c:1467
    8  execute_command () at gdb/top.c:476
    .....
    
    Possible solution is to add check to get_prev_frame_if_no_cycle, before it
    attempts to evaluate frame_id it should be confirmed that frame_id
    evaluation will not cause an assert.
    
    There could be several cases where this error might happen:
    
    1. Values of PC and return-PC has been set manually to malicious values.
    
    2. GDB connects to target that used to execute some application, but that
    wasn't completely reset, so register values are left in the state valid for
    previous application, but incidentaly cause described error in new application.
    
    3. Debug information is corrupted or doesn't match the application.
    
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- gdb/frame.c
+++ gdb/frame.c
@@ -1777,23 +1777,53 @@ get_prev_frame_if_no_cycle (struct frame_info *this_frame)
      linked onto THIS_FRAME.  */
   prev_frame_cleanup = make_cleanup (remove_prev_frame, this_frame);
 
-  compute_frame_id (prev_frame);
-  if (!frame_stash_add (prev_frame))
+  if (get_frame_type (prev_frame) == INLINE_FRAME)
     {
-      /* Another frame with the same id was already in the stash.  We just
-	 detected a cycle.  */
-      if (frame_debug)
-	{
-	  fprintf_unfiltered (gdb_stdlog, "-> ");
-	  fprint_frame (gdb_stdlog, NULL);
-	  fprintf_unfiltered (gdb_stdlog, " // this frame has same ID }\n");
-	}
-      this_frame->stop_reason = UNWIND_SAME_ID;
-      /* Unlink.  */
-      prev_frame->next = NULL;
-      this_frame->prev = NULL;
-      prev_frame = NULL;
+      /* This creates a recursion since get_prev_frame_always will eventually
+         call this function as well. Recursion will stops as soon as prev_frame
+         will be non-inlined (regardless of whether it will pass or fail the
+         further check for frame_id equality. */
+      struct frame_info *prev_prev_frame = get_prev_frame_always (prev_frame);
+      if (!prev_prev_frame) {
+	/* Previous frame is inline, however for some reason we cannot get a
+	   frame that be previous to it, and as a result it wouldn't be
+	   possible to evaluate frame_id of prev_frame. Some parts of GDB code
+	   assume that frame always have valid frame_id, so this function
+	   shouldn't return frame for which we know it will not be possible to
+	   evalute frame_id. */
+	if (frame_debug)
+	  {
+	    fprintf_unfiltered (gdb_stdlog, "-> ");
+	    fprint_frame (gdb_stdlog, NULL);
+	    fprintf_unfiltered (gdb_stdlog,
+				" // inline frame without previous frame }\n");
+	  }
+	this_frame->stop_reason = UNWIND_INNER_ID;
+	prev_frame->next = NULL;
+	this_frame->prev = NULL;
+	prev_frame = NULL;
+      }
     }
+
+  if (prev_frame) {
+    compute_frame_id (prev_frame);
+    if (!frame_stash_add (prev_frame))
+      {
+        /* Another frame with the same id was already in the stash.  We just
+	   detected a cycle.  */
+	if (frame_debug)
+	  {
+	    fprintf_unfiltered (gdb_stdlog, "-> ");
+	    fprint_frame (gdb_stdlog, NULL);
+	    fprintf_unfiltered (gdb_stdlog, " // this frame has same ID }\n");
+	  }
+	this_frame->stop_reason = UNWIND_SAME_ID;
+	/* Unlink.  */
+	prev_frame->next = NULL;
+	this_frame->prev = NULL;
+	prev_frame = NULL;
+      }
+  }
 
   discard_cleanups (prev_frame_cleanup);
   return prev_frame;
