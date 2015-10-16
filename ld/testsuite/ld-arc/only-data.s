06140449aaaa59a62bb657acd305f25f0e25bdea
Claudiu Zissulescu <claziss@synopsys.com>
2015-10-16 10:15:12 +0200

    Add empty archive tests

---
+++ ld/testsuite/ld-arc/only-data.s
@@ -0,0 +1,26 @@
+	.section	.debug_macro,"G",@progbits,barrier,comdat
+.Ldebug_macro151:
+	.2byte	0x4
+	.byte	0
+	.byte	0x5
+	.uleb128 0x12
+	.4byte	@.LASF4152
+	.byte	0x5
+	.uleb128 0x20
+	.4byte	@.LASF4153
+	.byte	0x5
+	.uleb128 0x24
+	.4byte	@.LASF4154
+	.byte	0x5
+	.uleb128 0x28
+	.4byte	@.LASF4155
+	.byte	0
+	.section	.debug_str,"MS",@progbits,1
+.LASF4152:
+	.string	"String 1"
+.LASF4153:
+	.string	"String 2"
+.LASF4154:
+	.string	"String 3"
+.LASF4155:
+	.string	"String 4"
