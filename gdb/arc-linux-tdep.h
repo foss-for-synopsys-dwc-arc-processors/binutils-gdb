b6b7f91b531ff0023304f32dd40350b90d7a76b4
Anton Kolesov <Anton.Kolesov@synopsys.com>
2016-07-05 21:57:42 +0300

    arc: Correct copyright header for upstreaming
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- gdb/arc-linux-tdep.h
+++ gdb/arc-linux-tdep.h
@@ -1,15 +1,13 @@
-/* Target dependent code for ARC processor family, for GDB, the GNU debugger.
+/* Target dependent code for GNU/Linux ARC.
 
-   Copyright 2014-2015 Synopsys Inc.
-
-   Authors:
-      Anton Kolesov <Anton.Kolesov@synopsys.com>
+   Copyright 2014-2016 Free Software Foundation, Inc.
+   Contributed by Synopsys Inc.
 
    This file is part of GDB.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
+   the Free Software Foundation; either version 3 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -18,12 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-*/
-
-/* This module provides support for the ARC processor family's target
-   dependencies which are specific to the user-space debugger. */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #ifndef ARC_LINUX_TDEP_H
 #define ARC_LINUX_TDEP_H
