ce419d82677180a5944510642e1e7419c05170c4
Anton Kolesov <Anton.Kolesov@synopsys.com>
2016-07-06 21:45:55 +0300

    xxx: Add vim tab specifications
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- gdb/arc-tdep.c
+++ gdb/arc-tdep.c
@@ -2204,3 +2204,5 @@ _initialize_arc_tdep (void)
 			    _("Non-zero enables ARC specific debugging."),
 			    NULL, NULL, &setdebuglist, &showdebuglist);
 }
+
+/* vim: set sts=2 shiftwidth=2 ts=8: */
