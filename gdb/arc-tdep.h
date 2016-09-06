70c83a4f18fcee32ae431705aaeab79c31dafc86
Anton Kolesov <Anton.Kolesov@synopsys.com>
2016-09-06 17:19:17 +0300

    arc: Recognize registers available on Linux targets
    For ARC there are registers that are not part of a required set in XML target
    descriptions by default, but which are almost always present on ARC targets and
    are universally exposed by the ptrace interface.  This patch adds those
    registers to those recognized by GDB:
    
    - R30 - core register available in user-space on ARC HS processors only (used
      to be a privileged only ILINK2 on ARC700).
    - R58, R59 - accumulator register pair for a 64-multiplier and
      double-precision FPU - only on ARC HS.
    - LP_START, LP_END - AUX registers, hardware loop start and end.  Formally
      optional, though it is hard to find ARC configuration that doesn't have
      them and is always present in processors that can run Linux.  GDB needs to
      know about those registers to implement proper software single stepping,
      since they affect what instruction will be next.
    - BTA - AUX register that contains branch target address.  Value of this
      register makes sense only when execution halts at the delay slot
      instruction - in this case branch instruction is already committed,
      STATUS32.DE is set to 1 and BTA contains address of next PC.  GDB needs to
      understand this register to properly handle situations when breakpoint has
      been set in the delay slot (delay slot is stepped over when doing software
      single stepping).  Unfortunately, right now this doesn't work very well,
      because Linux doesn't allow modifications of STATUS32 via ptrace and Linux
      uses TRAP_S instruction to implement software breakpoints - this
      instruction commits when executed, therefore when TRAP_S is set in the
      delay slot and execution halts at it, PC is already advanced to BTA value
      and STATUS32.DE is reset.  BTA register will be more useful for debugger
      after support for SWI instruction will be added to ARC Linux - this
      breakpoint instruction doesn't commit, hence it doesn't change processor
      state.
    
    gdb/ChangeLog:
    yyyy-mm-dd  Anton Kolesov  <Anton.Kolesov@synopsys.com>
    
    	* arc-tdep.c (core_v2_register_names): Fix names of R58 and R59.
    	(aux_minimal_register_names): Add LP_START, LP_END and BTA.
    	(arc_tdesc_init): Recognize those registers.
    	* arc-tdep.h (arc_regnum): Add R58, R59, LP_START, LP_END and BTA.
    	(gdbarch_tdep): New field has_hw_loops.
    
    gdb/doc/ChangeLog:
    yyyy-mm-dd  Anton Kolesov  <Anton.Kolesov@synopsys.com>
    
    	* gdb.texinfo (Synopsys ARC): Document LP_START, LP_END and BTA.

--- gdb/arc-tdep.h
+++ gdb/arc-tdep.h
@@ -53,6 +53,8 @@ enum arc_regnum
     ARC_R30_REGNUM,
     /* Return address from function.  */
     ARC_BLINK_REGNUM,
+    ARC_R58_REGNUM = 58,
+    ARC_R59_REGNUM,
     /* Zero-delay loop counter.  */
     ARC_LP_COUNT_REGNUM = 60,
     /* Reserved register number.  There should never be a register with such
@@ -68,14 +70,21 @@ enum arc_regnum
     /* Program counter, aligned to 4-bytes, read-only.  */
     ARC_PCL_REGNUM,
     ARC_LAST_CORE_REGNUM = ARC_PCL_REGNUM,
+
     /* AUX registers.  */
     /* Actual program counter.  */
     ARC_PC_REGNUM,
     ARC_FIRST_AUX_REGNUM = ARC_PC_REGNUM,
     /* Status register.  */
     ARC_STATUS32_REGNUM,
-    ARC_LAST_REGNUM = ARC_STATUS32_REGNUM,
-    ARC_LAST_AUX_REGNUM = ARC_STATUS32_REGNUM,
+    /* Zero-delay loop start instruction.  */
+    ARC_LP_START_REGNUM,
+    /* Zero-delay loop next-after-last instruction.  */
+    ARC_LP_END_REGNUM,
+    /* Branch target address.  */
+    ARC_BTA_REGNUM,
+    ARC_LAST_AUX_REGNUM = ARC_BTA_REGNUM,
+    ARC_LAST_REGNUM = ARC_LAST_AUX_REGNUM,
 
     /* Additional ABI constants.  */
     ARC_FIRST_ARG_REGNUM = ARC_R0_REGNUM,
@@ -100,6 +109,9 @@ struct gdbarch_tdep
   /* Offset to PC value in jump buffer.  If this is negative, longjmp
      support will be disabled.  */
   int jb_pc;
+
+  /* Whether target has hardware (aka zero-delay) loops.  */
+  bool has_hw_loops;
 };
 
 /* Utility functions used by other ARC-specific modules.  */
