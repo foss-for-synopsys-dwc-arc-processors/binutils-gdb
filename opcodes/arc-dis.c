3705c610a89db42041279fe26ace0590f87ae732
Claudiu Zissulescu <claziss@synopsys.com>
2017-07-19 11:05:00 +0200

    [ARC] Add support for QuarkSE2 EM.
    Add support in disassembler for QuarkSE2 EM cpu.
    
    include/
    2016-12-19  Claudiu Zissulescu  <claziss@synopsys.com>
    
    	* opcode/arc-attrs.h (FEATURE_LIST_NAME): Add QuarkSE1 and
    	QuarkSE2 features.
    	(CONFLICT_LIST): QuarkSE1 cannot mix with anything having FPU
    	extensions.
    	* opcode/arc.h (insn_subclass_t): Update enum for QuarkSE1 and
    	QuarkSE2 instructions.
    
    opcodes/
    2016-10-06  Claudiu Zissulescu  <claziss@synopsys.com>
    
    	* arc-dis.c (parse_option): Handle quarkse2_em option.  Update
    	quarkse option.
    	(print_arc_disassembler_options): Document quarkse2_em option.
    	* arc-ext-tbl.h (dsp_fp_flt2i, dsp_fp_i2flt): Only available for
    	QuarkSE1.
    	(dsp_fp_div, dsp_fp_cmp): Available also for QuarkSE2.
    
    binutils/
    2016-10-06  Claudiu Zissulescu  <claziss@synopsys.com>
    
    	* doc/binutils.texi (disassembler-options): Document quarkse2_em
    	option.
    
    ld/
    2016-12-19  Claudiu Zissulescu  <claziss@synopsys.com>
    
    	* testsuite/ld-arc/attr-merge-err-quarkse.d: New file.
    	* testsuite/ld-arc/attr-quarkse.s: Likewise.
    	* testsuite/ld-arc/attr-quarkse2.s: Likewise.

--- opcodes/arc-dis.c
+++ opcodes/arc-dis.c
@@ -761,6 +761,15 @@ parse_option (const char *option)
       add_to_decodelist (FLOAT, QUARKSE2);
     }
 
+  else if (disassembler_options_cmp (option, "quarkse2_em") == 0)
+    {
+      add_to_decodelist (DSP, NONE);
+      add_to_decodelist (FLOAT, DPA);
+      add_to_decodelist (FLOAT, SP);
+      add_to_decodelist (FLOAT, CVT);
+      add_to_decodelist (FLOAT, QUARKSE2);
+    }
+
   else if (disassembler_options_cmp (option, "fpuda") == 0)
     add_to_decodelist (FLOAT, DPA);
 
@@ -1404,6 +1413,8 @@ with -M switch (multiple options should be separated by commas):\n"));
   dpfp            Recognize FPX DP instructions.\n"));
   fprintf (stream, _("\
   quarkse_em      Recognize FPU QuarkSE-EM instructions.\n"));
+  fprintf (stream, _("\
+  quarkse2_em     Recognize FPU QuarkSE2-EM instructions.\n"));
   fprintf (stream, _("\
   fpuda           Recognize double assist FPU instructions.\n"));
   fprintf (stream, _("\
