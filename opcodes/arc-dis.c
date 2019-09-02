60a1f0e539b65d5dcd944e392291fabba574578a
Shahab Vahedi <shahab@synopsys.com>
2019-09-02 23:49:18 +0200

    [ARCx] Fix long immediate handling

--- opcodes/arc-dis.c
+++ opcodes/arc-dis.c
@@ -304,7 +304,7 @@ find_format_from_table (struct disassemble_info *info,
 
 	  /* Check for (short) LIMM indicator.  If it is there, then
 	     make sure we pick the right format.  */
-	  slimmind = (isa_mask & (ARC_OPCODE_ARCV2 || ARC_OPCODE_ARC64)) ?
+	  slimmind = (isa_mask & (ARC_OPCODE_ARCV2 | ARC_OPCODE_ARC64)) ?
 	    REG_LIMM_S : REG_LIMM;
 	  if (operand->flags & ARC_OPERAND_IR
 	      && !(operand->flags & ARC_OPERAND_LIMM))
