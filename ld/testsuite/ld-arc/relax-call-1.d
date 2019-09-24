29ff041cada11ac5c2aea7b5a4a5fe5faf12ac27
Claudiu Zissulescu <claziss@gmail.com>
2019-09-24 12:33:41 +0300

    [ARCx] Fix tests

--- ld/testsuite/ld-arc/relax-call-1.d
+++ ld/testsuite/ld-arc/relax-call-1.d
@@ -3,18 +3,18 @@
 #ld: -q -A elf32-arclittle -relax
 #objdump: -dr
 
-[^:]+:     file format elf32-.*arc
+[^:]+:     file format elf.*arc
 
 
 Disassembly of section \.text:
 
-00000100 <__start>:
- 100:	f801                	bl_s	4	;104 <foo>
-			100: R_ARC_S13_PCREL	foo
-			100: R_ARC_NONE	\*ABS\*\+0x4
- 102:	f801                	bl_s	4	;104 <foo>
-			102: R_ARC_S13_PCREL	foo
+[01]+ <__start>:
+\s+[0-9]+:	f801                	bl_s	4	;[0-9]+ <foo>
+			[0-9]+: R_ARC_S13_PCREL	foo
+			[0-9]+: R_ARC_NONE	\*ABS\*\+0x4
+\s+[0-9]+:	f801                	bl_s	4	;[0-9]+ <foo>
+			[0-9]+: R_ARC_S13_PCREL	foo
 
-00000104 <foo>:
- 104:	2000 0000           	add	r0,r0,r0
-			104: R_ARC_NONE	\*ABS\*\+0x6
+[0-9]+ <foo>:
+\s+[0-9]+:	2000 0000           	add	r0,r0,r0
+			[0-9]+: R_ARC_NONE	\*ABS\*\+0x6
