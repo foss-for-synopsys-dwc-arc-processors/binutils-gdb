7630728f849b869296eca0ecec8ed9d4b9230a46
Cupertino Miranda <cmiranda@synopsys.com>
2020-08-13 17:09:56 +0100

    Fix to later be verified. Maybe something more needs to be done in this context.

--- bfd/elfnn-arc.c
+++ bfd/elfnn-arc.c
@@ -2048,6 +2048,9 @@ elf_arc_check_relocs (bfd *			 abfd,
 	case R_ARC_32_ME:
 	case R_ARC_HI32_ME:
 	case R_ARC_LO32_ME:
+	  // TODO: ARC_PC32 reloc was moved here because busybox on ARC64
+	  //       needs a ARC_COPY reloc to lend an symbol from glibc.
+	case R_ARC_PC32:
 	  /* During shared library creation, these relocs should not
 	     appear in a shared library (as memory will be read only
 	     and the dynamic linker can not resolve these.  However
@@ -2096,7 +2099,6 @@ elf_arc_check_relocs (bfd *			 abfd,
 	    break;
 
 	  /* FALLTHROUGH */
-	case R_ARC_PC32:
 	case R_ARC_32_PCREL:
 	  if (!bfd_link_pic (info))
 	    break;
@@ -2441,6 +2443,8 @@ elf_arc_adjust_dynamic_symbol (struct bfd_link_info *info,
       BFD_ASSERT (def->root.type == bfd_link_hash_defined);
       h->root.u.def.section = def->root.u.def.section;
       h->root.u.def.value = def->root.u.def.value;
+      if (info->nocopyreloc)
+	h->non_got_ref = def->non_got_ref;
       return TRUE;
     }
 
