4a430300cc695a617b5c61e0afc7e86af5295b6f
Shahab Vahedi <shahab@synopsys.com>
2020-10-30 17:37:12 +0100

    arc64/gdb: Add very preliminary support

--- gdb/arch/arc.h
+++ gdb/arch/arc.h
@@ -24,7 +24,8 @@
 enum arc_isa
 {
   ARC_ISA_ARCV1 = 1,  /* a.k.a. ARCompact (ARC600, ARC700)  */
-  ARC_ISA_ARCV2	      /* such as ARC EM and ARC HS  */
+  ARC_ISA_ARCV2,      /* such as ARC EM and ARC HS  */
+  ARC_ISA_ARCV3	      /* both 32-bit and 64-bit variants  */
 };
 
 struct arc_arch_features
