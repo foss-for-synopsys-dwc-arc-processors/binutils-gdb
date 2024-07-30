c9b5c365f1efee0ebe786d67afadb1522484616a
Yuriy Kolerov <ykolerov@synopsys.com>
2024-07-30 17:58:50 +0300

    arc64: gdb: Add support of native GDB for ARCv3 32-bit targets
    This port includes support of reading core dumps and debugging
    Linux applications by native GDB for ARCv3 32-bit targets.
    
    A new file is added - arc64-linux-nat.c. It contains code for
    native Linux implementation of GDB. It is almost the same as
    arc-linux-nat.c for ARCv1/2 targets but with small changes
    related to ARCv3. Core dumps support also is almost the same
    as for ARCv2 except different registers order.
    
    Original structure and comments from ARCv2 implementation
    are preserved to simplify further merging of ARCv2 and ARCv3
    implementations.
    
    Signed-off-by: Yuriy Kolerov <ykolerov@synopsys.com>

--- gdb/arc64-linux-tdep.c
+++ gdb/arc64-linux-tdep.c
@@ -28,6 +28,7 @@
 
 /* ARC header files.  */
 #include "opcodes/arc-dis.h"
+#include "arc64-linux-tdep.h"
 #include "arc64-tdep.h"
 #include "arch/arc.h"
 
@@ -269,6 +270,217 @@ arc_linux_skip_solib_resolver (struct gdbarch *gdbarch, CORE_ADDR pc)
     }
 }
 
+static const int arc32_linux_gregset_offsets[] = {
+  ARC32_GREGSET_REGISTER_OFFSET (23),	/* R0 */
+  ARC32_GREGSET_REGISTER_OFFSET (22),	/* R1 */
+  ARC32_GREGSET_REGISTER_OFFSET (21),	/* R2 */
+  ARC32_GREGSET_REGISTER_OFFSET (20),	/* R3 */
+  ARC32_GREGSET_REGISTER_OFFSET (19),	/* R4 */
+  ARC32_GREGSET_REGISTER_OFFSET (18),	/* R5 */
+  ARC32_GREGSET_REGISTER_OFFSET (17),	/* R6 */
+  ARC32_GREGSET_REGISTER_OFFSET (16),	/* R7 */
+  ARC32_GREGSET_REGISTER_OFFSET (15),	/* R8 */
+  ARC32_GREGSET_REGISTER_OFFSET (14),	/* R9 */
+  ARC32_GREGSET_REGISTER_OFFSET (13),	/* R10 */
+  ARC32_GREGSET_REGISTER_OFFSET (12),	/* R11 */
+  ARC32_GREGSET_REGISTER_OFFSET (11),	/* R12 */
+  ARC32_GREGSET_REGISTER_OFFSET (10),	/* R13 */
+  ARC32_GREGSET_REGISTER_OFFSET (37),	/* R14 */
+  ARC32_GREGSET_REGISTER_OFFSET (36),	/* R15 */
+  ARC32_GREGSET_REGISTER_OFFSET (35),	/* R16 */
+  ARC32_GREGSET_REGISTER_OFFSET (34),	/* R17 */
+  ARC32_GREGSET_REGISTER_OFFSET (33),	/* R18 */
+  ARC32_GREGSET_REGISTER_OFFSET (32),	/* R19 */
+  ARC32_GREGSET_REGISTER_OFFSET (31),	/* R20 */
+  ARC32_GREGSET_REGISTER_OFFSET (30),	/* R21 */
+  ARC32_GREGSET_REGISTER_OFFSET (29),	/* R22 */
+  ARC32_GREGSET_REGISTER_OFFSET (28),	/* R23 */
+  ARC32_GREGSET_REGISTER_OFFSET (27),	/* R24 */
+  ARC32_GREGSET_REGISTER_OFFSET (26),	/* R25 */
+  ARC_OFFSET_NO_REGISTER,		/* R26 */
+  ARC32_GREGSET_REGISTER_OFFSET (8),	/* R27 - FP */
+  ARC32_GREGSET_REGISTER_OFFSET (24),	/* R28 - SP */
+  ARC_OFFSET_NO_REGISTER,		/* R29 - ILINK */
+  ARC32_GREGSET_REGISTER_OFFSET (9),	/* R30 - GP */
+  ARC32_GREGSET_REGISTER_OFFSET (7),	/* R31 - BLINK */
+  ARC_OFFSET_NO_REGISTER,		/* R32 */
+  ARC_OFFSET_NO_REGISTER,		/* R33 */
+  ARC_OFFSET_NO_REGISTER,		/* R34 */
+  ARC_OFFSET_NO_REGISTER,		/* R35 */
+  ARC_OFFSET_NO_REGISTER,		/* R36 */
+  ARC_OFFSET_NO_REGISTER,		/* R37 */
+  ARC_OFFSET_NO_REGISTER,		/* R38 */
+  ARC_OFFSET_NO_REGISTER,		/* R39 */
+  ARC_OFFSET_NO_REGISTER,		/* R40 */
+  ARC_OFFSET_NO_REGISTER,		/* R41 */
+  ARC_OFFSET_NO_REGISTER,		/* R42 */
+  ARC_OFFSET_NO_REGISTER,		/* R43 */
+  ARC_OFFSET_NO_REGISTER,		/* R44 */
+  ARC_OFFSET_NO_REGISTER,		/* R45 */
+  ARC_OFFSET_NO_REGISTER,		/* R46 */
+  ARC_OFFSET_NO_REGISTER,		/* R47 */
+  ARC_OFFSET_NO_REGISTER,		/* R48 */
+  ARC_OFFSET_NO_REGISTER,		/* R49 */
+  ARC_OFFSET_NO_REGISTER,		/* R50 */
+  ARC_OFFSET_NO_REGISTER,		/* R51 */
+  ARC_OFFSET_NO_REGISTER,		/* R52 */
+  ARC_OFFSET_NO_REGISTER,		/* R53 */
+  ARC_OFFSET_NO_REGISTER,		/* R54 */
+  ARC_OFFSET_NO_REGISTER,		/* R55 */
+  ARC_OFFSET_NO_REGISTER,		/* R56 */
+  ARC_OFFSET_NO_REGISTER,		/* R57 */
+  ARC_OFFSET_NO_REGISTER,		/* R58 */
+  ARC_OFFSET_NO_REGISTER,		/* R59 */
+  ARC_OFFSET_NO_REGISTER,		/* R60 - LP_COUNT */
+  ARC_OFFSET_NO_REGISTER,		/* R61 - RESERVED */
+  ARC_OFFSET_NO_REGISTER,		/* R62 - LIMM */
+  ARC_OFFSET_NO_REGISTER,		/* R63 - PCL */
+  ARC_OFFSET_NO_REGISTER,		/* F0 */
+  ARC_OFFSET_NO_REGISTER,		/* F1 */
+  ARC_OFFSET_NO_REGISTER,		/* F2 */
+  ARC_OFFSET_NO_REGISTER,		/* F3 */
+  ARC_OFFSET_NO_REGISTER,		/* F4 */
+  ARC_OFFSET_NO_REGISTER,		/* F5 */
+  ARC_OFFSET_NO_REGISTER,		/* F6 */
+  ARC_OFFSET_NO_REGISTER,		/* F7 */
+  ARC_OFFSET_NO_REGISTER,		/* F8 */
+  ARC_OFFSET_NO_REGISTER,		/* F9 */
+  ARC_OFFSET_NO_REGISTER,		/* F10 */
+  ARC_OFFSET_NO_REGISTER,		/* F11 */
+  ARC_OFFSET_NO_REGISTER,		/* F12 */
+  ARC_OFFSET_NO_REGISTER,		/* F13 */
+  ARC_OFFSET_NO_REGISTER,		/* F14 */
+  ARC_OFFSET_NO_REGISTER,		/* F15 */
+  ARC_OFFSET_NO_REGISTER,		/* F16 */
+  ARC_OFFSET_NO_REGISTER,		/* F17 */
+  ARC_OFFSET_NO_REGISTER,		/* F18 */
+  ARC_OFFSET_NO_REGISTER,		/* F19 */
+  ARC_OFFSET_NO_REGISTER,		/* F20 */
+  ARC_OFFSET_NO_REGISTER,		/* F21 */
+  ARC_OFFSET_NO_REGISTER,		/* F22 */
+  ARC_OFFSET_NO_REGISTER,		/* F23 */
+  ARC_OFFSET_NO_REGISTER,		/* F24 */
+  ARC_OFFSET_NO_REGISTER,		/* F25 */
+  ARC_OFFSET_NO_REGISTER,		/* F26 */
+  ARC_OFFSET_NO_REGISTER,		/* F27 */
+  ARC_OFFSET_NO_REGISTER,		/* F28 */
+  ARC_OFFSET_NO_REGISTER,		/* F29 */
+  ARC_OFFSET_NO_REGISTER,		/* F30 */
+  ARC_OFFSET_NO_REGISTER,		/* F31 */
+  ARC32_GREGSET_REGISTER_OFFSET (39),	/* PC */
+  ARC32_GREGSET_REGISTER_OFFSET (5),	/* STATUS32 */
+  ARC32_GREGSET_REGISTER_OFFSET (1),	/* BTA */
+  ARC32_GREGSET_REGISTER_OFFSET (6)	/* ERET */
+};
+
+/* Populate REGCACHE with register REGNUM from BUF.  */
+
+static void
+supply_register (const struct regset *regset,
+		 struct regcache *regcache,
+		 int regnum,
+		 const gdb_byte *buf)
+{
+  const int *offsets = (const int *) regset->regmap;
+
+  /* Skip non-existing registers.  */
+  if ((offsets[regnum] == ARC_OFFSET_NO_REGISTER))
+    return;
+
+  regcache->raw_supply (regnum, buf + offsets[regnum]);
+}
+
+void
+arc_linux_supply_gregset (const struct regset *regset,
+			  struct regcache *regcache,
+			  int regnum, const void *gregs, size_t size)
+{
+  const bfd_byte *buf = (const bfd_byte *) gregs;
+
+  /* REGNUM == -1 means writing all the registers.  */
+  if (regnum == -1)
+    for (int reg = 0; reg <= ARC_LAST_REGNUM; reg++)
+      supply_register (regset, regcache, reg, buf);
+  else if (regnum <= ARC_LAST_REGNUM)
+    supply_register (regset, regcache, regnum, buf);
+  else
+    gdb_assert_not_reached ("Invalid regnum in arc_linux_supply_gregset.");
+}
+
+/* Populate BUF with register REGNUM from the REGCACHE.  */
+
+static void
+collect_register (const struct regset *regset,
+		  const struct regcache *regcache,
+		  struct gdbarch *gdbarch,
+		  int regnum,
+		  gdb_byte *buf)
+{
+  const int *offsets = (const int *) regset->regmap;
+  int offset;
+
+  /* Skip non-existing registers.  */
+  if (offsets[regnum] == ARC_OFFSET_NO_REGISTER)
+    return;
+
+  /* The address where the execution has stopped is in pseudo-register
+     STOP_PC.  However, when kernel code is returning from the exception,
+     it uses the value from ERET register.  Since, TRAP_S (the breakpoint
+     instruction) commits, the ERET points to the next instruction.  In
+     other words: ERET != STOP_PC.  To jump back from the kernel code to
+     the correct address, ERET must be overwritten by GDB's STOP_PC.  Else,
+     the program will continue at the address after the current instruction.
+     */
+  if (regnum == gdbarch_pc_regnum (gdbarch))
+    offset = offsets[ARC_ERET_REGNUM];
+  else
+    offset = offsets[regnum];
+  regcache->raw_collect (regnum, buf + offset);
+}
+
+void
+arc_linux_collect_gregset (const struct regset *regset,
+			   const struct regcache *regcache,
+			   int regnum, void *gregs, size_t size)
+{
+  gdb_byte *buf = (gdb_byte *) gregs;
+  struct gdbarch *gdbarch = regcache->arch ();
+
+  /* REGNUM == -1 means writing all the registers.  */
+  if (regnum == -1)
+    for (int reg = 0; reg <= ARC_LAST_REGNUM; reg++)
+      collect_register (regset, regcache, gdbarch, reg, buf);
+  else if (regnum <= ARC_LAST_REGNUM)
+    collect_register (regset, regcache, gdbarch, regnum, buf);
+  else
+    gdb_assert_not_reached ("Invalid regnum in arc_linux_collect_gregset.");
+}
+
+/* Linux regset definitions.  */
+
+const struct regset arc32_linux_gregset = {
+  arc32_linux_gregset_offsets,
+  arc_linux_supply_gregset,
+  arc_linux_collect_gregset,
+};
+
+/* Implement the `iterate_over_regset_sections` gdbarch method.  */
+
+static void
+arc32_linux_iterate_over_regset_sections (struct gdbarch *gdbarch,
+					  iterate_over_regset_sections_cb *cb,
+					  void *cb_data,
+					  const struct regcache *regcache)
+{
+  static_assert (ARC_LAST_REGNUM < ARRAY_SIZE (arc32_linux_gregset_offsets));
+
+  const int sizeof_gregset =
+	ARC_GREGSET_REGISTERS_NUMBER * arc64_isa_reg_size (gdbarch);
+
+  cb (".reg", sizeof_gregset, sizeof_gregset, &arc32_linux_gregset, NULL,
+      cb_data);
+}
+
 /* Initialization specific to Linux environment.  */
 
 static void
@@ -301,6 +513,13 @@ arc64_linux_init_osabi (struct gdbarch_info info, struct gdbarch *gdbarch)
   set_gdbarch_software_single_step (gdbarch, arc_linux_software_single_step);
   set_gdbarch_skip_trampoline_code (gdbarch, find_solib_trampoline_target);
   set_gdbarch_skip_solib_resolver (gdbarch, arc_linux_skip_solib_resolver);
+
+  // TODO: Reading of a core dump is not supported by HS6x yet.
+  if (arc64_isa_reg_size (gdbarch) == 4)
+    {
+      set_gdbarch_iterate_over_regset_sections
+	(gdbarch, arc32_linux_iterate_over_regset_sections);
+    }
 
   /* GNU/Linux uses SVR4-style shared libraries...  */
   set_solib_svr4_fetch_link_map_offsets (gdbarch,
