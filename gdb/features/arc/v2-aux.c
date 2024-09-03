2c6f92249ae008a0bf3ff058c5b07f63ddb96ec8
Yuriy Kolerov <ykolerov@synopsys.com>
2024-09-03 14:12:14 +0300

    arc: gdb: Add ERET register to "features" files
    This register was not listed in "features" files and was not
    available for examination through "info registers".
    
    Signed-off-by: Yuriy Kolerov <ykolerov@synopsys.com>

--- gdb/features/arc/v2-aux.c
+++ gdb/features/arc/v2-aux.c
@@ -34,5 +34,6 @@ create_feature_arc_v2_aux (struct target_desc *result, long regnum)
   tdesc_create_reg (feature, "lp_start", regnum++, 1, NULL, 32, "code_ptr");
   tdesc_create_reg (feature, "lp_end", regnum++, 1, NULL, 32, "code_ptr");
   tdesc_create_reg (feature, "bta", regnum++, 1, NULL, 32, "code_ptr");
+  tdesc_create_reg (feature, "eret", regnum++, 1, NULL, 32, "code_ptr");
   return regnum;
 }
