22fcb828436e05e6bbe537cd097c22f5a13b118d
Luis Silva <luiss@synopsys.com>
2025-05-19 03:24:19 -0700

    fixup! tmp: arc{32,64}: Add floating-point instruction testing.

--- gas/testsuite/gas/arc64/fmvi2s.d
+++ gas/testsuite/gas/arc64/fmvi2s.d
@@ -5,35 +5,36 @@
 .*: +file format elf32-.*arc64
 
 Disassembly of section .text:
-0x[0-9a-f]+ e0e0 0030\s{2,}fmvi2s\tf0,r0
-0x[0-9a-f]+ e1e0 0070\s{2,}fmvi2s\tf1,r1
-0x[0-9a-f]+ e2e0 00b0\s{2,}fmvi2s\tf2,r2
-0x[0-9a-f]+ e3e0 00f0\s{2,}fmvi2s\tf3,r3
-0x[0-9a-f]+ e4e0 0130\s{2,}fmvi2s\tf4,r4
-0x[0-9a-f]+ e5e0 0170\s{2,}fmvi2s\tf5,r5
-0x[0-9a-f]+ e6e0 01b0\s{2,}fmvi2s\tf6,r6
-0x[0-9a-f]+ e7e0 01f0\s{2,}fmvi2s\tf7,r7
-0x[0-9a-f]+ e0e0 1230\s{2,}fmvi2s\tf8,r8
-0x[0-9a-f]+ e1e0 1270\s{2,}fmvi2s\tf9,r9
-0x[0-9a-f]+ e2e0 12b0\s{2,}fmvi2s\tf10,r10
-0x[0-9a-f]+ e3e0 12f0\s{2,}fmvi2s\tf11,r11
-0x[0-9a-f]+ e4e0 1330\s{2,}fmvi2s\tf12,r12
-0x[0-9a-f]+ e5e0 1370\s{2,}fmvi2s\tf13,r13
-0x[0-9a-f]+ e6e0 13b0\s{2,}fmvi2s\tf14,r14
-0x[0-9a-f]+ e7e0 13f0\s{2,}fmvi2s\tf15,r15
-0x[0-9a-f]+ e0e0 2430\s{2,}fmvi2s\tf16,r16
-0x[0-9a-f]+ e1e0 2470\s{2,}fmvi2s\tf17,r17
-0x[0-9a-f]+ e2e0 24b0\s{2,}fmvi2s\tf18,r18
-0x[0-9a-f]+ e3e0 24f0\s{2,}fmvi2s\tf19,r19
-0x[0-9a-f]+ e4e0 2530\s{2,}fmvi2s\tf20,r20
-0x[0-9a-f]+ e5e0 2570\s{2,}fmvi2s\tf21,r21
-0x[0-9a-f]+ e6e0 25b0\s{2,}fmvi2s\tf22,r22
-0x[0-9a-f]+ e7e0 25f0\s{2,}fmvi2s\tf23,r23
-0x[0-9a-f]+ e0e0 3630\s{2,}fmvi2s\tf24,r24
-0x[0-9a-f]+ e1e0 3670\s{2,}fmvi2s\tf25,r25
-0x[0-9a-f]+ e2e0 36b0\s{2,}fmvi2s\tf26,r26
-0x[0-9a-f]+ e3e0 36f0\s{2,}fmvi2s\tf27,fp
-0x[0-9a-f]+ e4e0 3730\s{2,}fmvi2s\tf28,sp
-0x[0-9a-f]+ e5e0 3770\s{2,}fmvi2s\tf29,ilink
-0x[0-9a-f]+ e6e0 37b0\s{2,}fmvi2s\tf30,r30
-0x[0-9a-f]+ e7e0 37f0\s{2,}fmvi2s\tf31,blink
+0x[0-9a-f]+ e0e0 0030           	fmvi2s	f0,r0
+0x[0-9a-f]+ e1e0 0070           	fmvi2s	f1,r1
+0x[0-9a-f]+ e2e0 00b0           	fmvi2s	f2,r2
+0x[0-9a-f]+ e3e0 00f0           	fmvi2s	f3,r3
+0x[0-9a-f]+ e4e0 0130           	fmvi2s	f4,r4
+0x[0-9a-f]+ e5e0 0170           	fmvi2s	f5,r5
+0x[0-9a-f]+ e6e0 01b0           	fmvi2s	f6,r6
+0x[0-9a-f]+ e7e0 01f0           	fmvi2s	f7,r7
+0x[0-9a-f]+ e0e0 1230           	fmvi2s	f8,r8
+0x[0-9a-f]+ e1e0 1270           	fmvi2s	f9,r9
+0x[0-9a-f]+ e2e0 12b0           	fmvi2s	f10,r10
+0x[0-9a-f]+ e3e0 12f0           	fmvi2s	f11,r11
+0x[0-9a-f]+ e4e0 1330           	fmvi2s	f12,r12
+0x[0-9a-f]+ e5e0 1370           	fmvi2s	f13,r13
+0x[0-9a-f]+ e6e0 13b0           	fmvi2s	f14,r14
+0x[0-9a-f]+ e7e0 13f0           	fmvi2s	f15,r15
+0x[0-9a-f]+ e0e0 2430           	fmvi2s	f16,r16
+0x[0-9a-f]+ e1e0 2470           	fmvi2s	f17,r17
+0x[0-9a-f]+ e2e0 24b0           	fmvi2s	f18,r18
+0x[0-9a-f]+ e3e0 24f0           	fmvi2s	f19,r19
+0x[0-9a-f]+ e4e0 2530           	fmvi2s	f20,r20
+0x[0-9a-f]+ e5e0 2570           	fmvi2s	f21,r21
+0x[0-9a-f]+ e6e0 25b0           	fmvi2s	f22,r22
+0x[0-9a-f]+ e7e0 25f0           	fmvi2s	f23,r23
+0x[0-9a-f]+ e0e0 3630           	fmvi2s	f24,r24
+0x[0-9a-f]+ e1e0 3670           	fmvi2s	f25,r25
+0x[0-9a-f]+ e2e0 36b0           	fmvi2s	f26,r26
+0x[0-9a-f]+ e3e0 36f0           	fmvi2s	f27,fp
+0x[0-9a-f]+ e4e0 3730           	fmvi2s	f28,sp
+0x[0-9a-f]+ e5e0 3770           	fmvi2s	f29,ilink
+0x[0-9a-f]+ e6e0 37b0           	fmvi2s	f30,r30
+0x[0-9a-f]+ e7e0 37f0           	fmvi2s	f31,blink
+
--- gas/testsuite/gas/arc64/fmvs2i.d
+++ gas/testsuite/gas/arc64/fmvs2i.d
@@ -5,35 +5,35 @@
 .*: +file format elf32-.*arc64
 
 Disassembly of section .text:
-0x[0-9a-f]+ e060 0031\s{2,}\tfmvs2i\tr0,f0
-0x[0-9a-f]+ e160 0071\s{2,}\tfmvs2i\tr1,f1
-0x[0-9a-f]+ e260 00b1\s{2,}\tfmvs2i\tr2,f2
-0x[0-9a-f]+ e360 00f1\s{2,}\tfmvs2i\tr3,f3
-0x[0-9a-f]+ e460 0131\s{2,}\tfmvs2i\tr4,f4
-0x[0-9a-f]+ e560 0171\s{2,}\tfmvs2i\tr5,f5
-0x[0-9a-f]+ e660 01b1\s{2,}\tfmvs2i\tr6,f6
-0x[0-9a-f]+ e760 01f1\s{2,}\tfmvs2i\tr7,f7
-0x[0-9a-f]+ e060 1231\s{2,}\tfmvs2i\tr8,f8
-0x[0-9a-f]+ e160 1271\s{2,}\tfmvs2i\tr9,f9
-0x[0-9a-f]+ e260 12b1\s{2,}\tfmvs2i\tr10,f10
-0x[0-9a-f]+ e360 12f1\s{2,}\tfmvs2i\tr11,f11
-0x[0-9a-f]+ e460 1331\s{2,}\tfmvs2i\tr12,f12
-0x[0-9a-f]+ e560 1371\s{2,}\tfmvs2i\tr13,f13
-0x[0-9a-f]+ e660 13b1\s{2,}\tfmvs2i\tr14,f14
-0x[0-9a-f]+ e760 13f1\s{2,}\tfmvs2i\tr15,f15
-0x[0-9a-f]+ e060 2431\s{2,}\tfmvs2i\tr16,f16
-0x[0-9a-f]+ e160 2471\s{2,}\tfmvs2i\tr17,f17
-0x[0-9a-f]+ e260 24b1\s{2,}\tfmvs2i\tr18,f18
-0x[0-9a-f]+ e360 24f1\s{2,}\tfmvs2i\tr19,f19
-0x[0-9a-f]+ e460 2531\s{2,}\tfmvs2i\tr20,f20
-0x[0-9a-f]+ e560 2571\s{2,}\tfmvs2i\tr21,f21
-0x[0-9a-f]+ e660 25b1\s{2,}\tfmvs2i\tr22,f22
-0x[0-9a-f]+ e760 25f1\s{2,}\tfmvs2i\tr23,f23
-0x[0-9a-f]+ e060 3631\s{2,}\tfmvs2i\tr24,f24
-0x[0-9a-f]+ e160 3671\s{2,}\tfmvs2i\tr25,f25
-0x[0-9a-f]+ e260 36b1\s{2,}\tfmvs2i\tr26,f26
-0x[0-9a-f]+ e360 36f1\s{2,}\tfmvs2i\tfp,f27
-0x[0-9a-f]+ e460 3731\s{2,}\tfmvs2i\tsp,f28
-0x[0-9a-f]+ e560 3771\s{2,}\tfmvs2i\tilink,f29
-0x[0-9a-f]+ e660 37b1\s{2,}\tfmvs2i\tr30,f30
-0x[0-9a-f]+ e760 37f1\s{2,}\tfmvs2i\tblink,f31
+0x[0-9a-f]+ e060 0031           	fmvs2i	r0,f0
+0x[0-9a-f]+ e160 0071           	fmvs2i	r1,f1
+0x[0-9a-f]+ e260 00b1           	fmvs2i	r2,f2
+0x[0-9a-f]+ e360 00f1           	fmvs2i	r3,f3
+0x[0-9a-f]+ e460 0131           	fmvs2i	r4,f4
+0x[0-9a-f]+ e560 0171           	fmvs2i	r5,f5
+0x[0-9a-f]+ e660 01b1           	fmvs2i	r6,f6
+0x[0-9a-f]+ e760 01f1           	fmvs2i	r7,f7
+0x[0-9a-f]+ e060 1231           	fmvs2i	r8,f8
+0x[0-9a-f]+ e160 1271           	fmvs2i	r9,f9
+0x[0-9a-f]+ e260 12b1           	fmvs2i	r10,f10
+0x[0-9a-f]+ e360 12f1           	fmvs2i	r11,f11
+0x[0-9a-f]+ e460 1331           	fmvs2i	r12,f12
+0x[0-9a-f]+ e560 1371           	fmvs2i	r13,f13
+0x[0-9a-f]+ e660 13b1           	fmvs2i	r14,f14
+0x[0-9a-f]+ e760 13f1           	fmvs2i	r15,f15
+0x[0-9a-f]+ e060 2431           	fmvs2i	r16,f16
+0x[0-9a-f]+ e160 2471           	fmvs2i	r17,f17
+0x[0-9a-f]+ e260 24b1           	fmvs2i	r18,f18
+0x[0-9a-f]+ e360 24f1           	fmvs2i	r19,f19
+0x[0-9a-f]+ e460 2531           	fmvs2i	r20,f20
+0x[0-9a-f]+ e560 2571           	fmvs2i	r21,f21
+0x[0-9a-f]+ e660 25b1           	fmvs2i	r22,f22
+0x[0-9a-f]+ e760 25f1           	fmvs2i	r23,f23
+0x[0-9a-f]+ e060 3631           	fmvs2i	r24,f24
+0x[0-9a-f]+ e160 3671           	fmvs2i	r25,f25
+0x[0-9a-f]+ e260 36b1           	fmvs2i	r26,f26
+0x[0-9a-f]+ e360 36f1           	fmvs2i	fp,f27
+0x[0-9a-f]+ e460 3731           	fmvs2i	sp,f28
+0x[0-9a-f]+ e560 3771           	fmvs2i	ilink,f29
+0x[0-9a-f]+ e660 37b1           	fmvs2i	r30,f30
+0x[0-9a-f]+ e760 37f1           	fmvs2i	blink,f31
