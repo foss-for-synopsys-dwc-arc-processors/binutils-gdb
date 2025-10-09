7385208457d14eca3d7be8d51b0d2ac2dcd28fb6
Luis Silva <luiss@synopsys.com>
2025-10-09 17:03:01 +0100

    arcv: Fix operand order for several ARC-V vDSP instructions.
    Some ARC-V vector instructions had their operands incorrectly ordered,
    resulting in wrong encodings being generated.  Others had operands that
    were interchangeable but have now been aligned with the default
    ordering defined in the ARC-V vDSP specification.
    
    This patch corrects the operand order for affected opcodes in
    riscv-opc.c and updates the testsuite accordingly to validate the
    correct encodings.
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- gas/testsuite/gas/riscv/x-arcv-mxmc.d
+++ gas/testsuite/gas/riscv/x-arcv-mxmc.d
@@ -7,12 +7,12 @@
 Disassembly of section .text:
 
 0+[0-9a-f]+ <.text>:
-[ 	]+[0-9a-f]+:[ 	]+0a33205b[ 	]+arcv.vqmxm8.vv[ 	]+v0,v3,v6
-[ 	]+[0-9a-f]+:[ 	]+0a6021db[ 	]+arcv.vqmxm8.vv[ 	]+v3,v6,v0
-[ 	]+[0-9a-f]+:[ 	]+0a01a35b[ 	]+arcv.vqmxm8.vv[ 	]+v6,v0,v3
-[ 	]+[0-9a-f]+:[ 	]+0a33605b[ 	]+arcv.vqmxm8u.vv[ 	]+v0,v3,v6
-[ 	]+[0-9a-f]+:[ 	]+0a6061db[ 	]+arcv.vqmxm8u.vv[ 	]+v3,v6,v0
-[ 	]+[0-9a-f]+:[ 	]+0a01e35b[ 	]+arcv.vqmxm8u.vv[ 	]+v6,v0,v3
-[ 	]+[0-9a-f]+:[ 	]+0a33105b[ 	]+arcv.vqmxm8su.vv[ 	]+v0,v3,v6
-[ 	]+[0-9a-f]+:[ 	]+0a6011db[ 	]+arcv.vqmxm8su.vv[ 	]+v3,v6,v0
-[ 	]+[0-9a-f]+:[ 	]+0a01935b[ 	]+arcv.vqmxm8su.vv[ 	]+v6,v0,v3
+[ 	]+[0-9a-f]+:[ 	]+0a61a05b[ 	]+arcv.vqmxm8.vv[ 	]+v0,v3,v6
+[ 	]+[0-9a-f]+:[ 	]+0a0321db[ 	]+arcv.vqmxm8.vv[ 	]+v3,v6,v0
+[ 	]+[0-9a-f]+:[ 	]+0a30235b[ 	]+arcv.vqmxm8.vv[ 	]+v6,v0,v3
+[ 	]+[0-9a-f]+:[ 	]+0a61e05b[ 	]+arcv.vqmxm8u.vv[ 	]+v0,v3,v6
+[ 	]+[0-9a-f]+:[ 	]+0a0361db[ 	]+arcv.vqmxm8u.vv[ 	]+v3,v6,v0
+[ 	]+[0-9a-f]+:[ 	]+0a30635b[ 	]+arcv.vqmxm8u.vv[ 	]+v6,v0,v3
+[ 	]+[0-9a-f]+:[ 	]+0a61905b[ 	]+arcv.vqmxm8su.vv[ 	]+v0,v3,v6
+[ 	]+[0-9a-f]+:[ 	]+0a0311db[ 	]+arcv.vqmxm8su.vv[ 	]+v3,v6,v0
+[ 	]+[0-9a-f]+:[ 	]+0a30135b[ 	]+arcv.vqmxm8su.vv[ 	]+v6,v0,v3
