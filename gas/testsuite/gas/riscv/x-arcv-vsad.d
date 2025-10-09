f563984c708fdd063449f9e940f12f44df7c88b1
Luis Silva <luiss@synopsys.com>
2025-10-09 17:03:01 +0100

    arcv: Fix operand order for several ARC-V vDSP instructions.
    Some ARC-V vector instructions had their operands incorrectly ordered,
    resulting in wrong encodings being generated.  Others had operands that
    were interchangeable but have now been aligned with the default
    ordering defined in the ARC-V vDSP specification.
    
    This patch corrects the operand order for affected opcodes in
    riscv-opc.c and updates the testsuite accordingly to validate the
    correct encodings.
    
    Signed-off-by: Luis Silva <luiss@synopsys.com>

--- gas/testsuite/gas/riscv/x-arcv-vsad.d
+++ gas/testsuite/gas/riscv/x-arcv-vsad.d
@@ -7,9 +7,9 @@
 Disassembly of section .text:
 
 0+[0-9a-f]+ <.text>:
-[ 	]+[0-9a-f]+:[ 	]+c233205b[ 	]+arcv.vwsad.vv[ 	]+v0,v3,v6
-[ 	]+[0-9a-f]+:[ 	]+c26021db[ 	]+arcv.vwsad.vv[ 	]+v3,v6,v0
-[ 	]+[0-9a-f]+:[ 	]+c201a35b[ 	]+arcv.vwsad.vv[ 	]+v6,v0,v3
-[ 	]+[0-9a-f]+:[ 	]+c233605b[ 	]+arcv.vwsadu.vv[ 	]+v0,v3,v6
-[ 	]+[0-9a-f]+:[ 	]+c26061db[ 	]+arcv.vwsadu.vv[ 	]+v3,v6,v0
-[ 	]+[0-9a-f]+:[ 	]+c201e35b[ 	]+arcv.vwsadu.vv[ 	]+v6,v0,v3
+[ 	]+[0-9a-f]+:[ 	]+c261a05b[ 	]+arcv.vwsad.vv[ 	]+v0,v3,v6
+[ 	]+[0-9a-f]+:[ 	]+c20321db[ 	]+arcv.vwsad.vv[ 	]+v3,v6,v0
+[ 	]+[0-9a-f]+:[ 	]+c230235b[ 	]+arcv.vwsad.vv[ 	]+v6,v0,v3
+[ 	]+[0-9a-f]+:[ 	]+c261e05b[ 	]+arcv.vwsadu.vv[ 	]+v0,v3,v6
+[ 	]+[0-9a-f]+:[ 	]+c20361db[ 	]+arcv.vwsadu.vv[ 	]+v3,v6,v0
+[ 	]+[0-9a-f]+:[ 	]+c230635b[ 	]+arcv.vwsadu.vv[ 	]+v6,v0,v3
