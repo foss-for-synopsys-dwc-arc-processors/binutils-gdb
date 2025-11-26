1c854fb98a9d955ed4f7dd28101a2187d83f1b24
Michiel Derhaeg <michiel@synopsys.com>
2025-11-26 15:34:50 +0100

    arcv: update bspeek and bspop encoding to reflect the XARCV 1.8 spec
    Signed-off-by: Michiel Derhaeg <michiel@synopsys.com>

--- gas/testsuite/gas/riscv/x-arcv-bitstream.d
+++ gas/testsuite/gas/riscv/x-arcv-bitstream.d
@@ -7,10 +7,10 @@
 Disassembly of section .text:
 
 0+[0-9a-f]+ <.text>:
-[ 	]+[0-9a-f]+:[ 	]+49c222db[ 	]+arcv.bspeek[ 	]+t0,t3
-[ 	]+[0-9a-f]+:[ 	]+49f22e5b[ 	]+arcv.bspeek[ 	]+t3,t6
-[ 	]+[0-9a-f]+:[ 	]+49c2a2db[ 	]+arcv.bspop[ 	]+t0,t3
-[ 	]+[0-9a-f]+:[ 	]+49f2ae5b[ 	]+arcv.bspop[ 	]+t3,t6
+[ 	]+[0-9a-f]+:[ 	]+484e12db[ 	]+arcv.bspeek[ 	]+t0,t3
+[ 	]+[0-9a-f]+:[ 	]+484f9e5b[ 	]+arcv.bspeek[ 	]+t3,t6
+[ 	]+[0-9a-f]+:[ 	]+485e12db[ 	]+arcv.bspop[ 	]+t0,t3
+[ 	]+[0-9a-f]+:[ 	]+485f9e5b[ 	]+arcv.bspop[ 	]+t3,t6
 [ 	]+[0-9a-f]+:[ 	]+01fe12db[ 	]+arcv.bspush[ 	]+t0,t3,t6
 [ 	]+[0-9a-f]+:[ 	]+005f9e5b[ 	]+arcv.bspush[ 	]+t3,t6,t0
 [ 	]+[0-9a-f]+:[ 	]+01c29fdb[ 	]+arcv.bspush[ 	]+t6,t0,t3
